bbc6ac28d46f5e228cbdf3282c764cf10795c7ee
Min0000Young <anniealsdud@naver.com>
2023-07-29 18:58:48 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pickick/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.pickick;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    TextView welcomeTextView;
+    EditText messageEditText;
+    ImageButton sendButton;
+    List<Message> messageList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        messageList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recycler_view);
+        welcomeTextView = findViewById(R.id.welcome_text);
+        messageEditText = findViewById(R.id.message_edit_text);
+        sendButton = findViewById(R.id.send_btn);
+
+        sendButton.setOnClickListener((v)->{
+           String question = messageEditText.getText().toString().trim();
+           Toast.makeText(this, question, Toast.LENGTH_LONG).show();
+        });
+    }
+}
